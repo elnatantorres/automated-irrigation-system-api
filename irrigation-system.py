184137043fe15d632fd8df42d634e3d8b1477685
yurimoyses09 <yuri09.moyses@gmail.com>
2020-05-31 15:00:38 -0300

    controller arduino

--- irrigation-system.py
+++ irrigation-system.py
@@ -2,8 +2,8 @@ import time
 from pyfirmata import Arduino, util
 
 Board = Arduino('COM4')  # Define a porta onde esta o arduino
-it = util.Iterator(Board)  # Controla as leituras das portas do arduino
-it.start()
+it = util.Iterator(Board)  # Controls arduino port readings
+it.start()  # Starts communication with the arduino.
 
 # Define pino do rele como OUTPUT
 pino_rele = Board.get_pin('d:10:o')
@@ -19,17 +19,17 @@ while True:
     valor_analogico = valor_analog.read()  # Valor que o sensor leu
 
     if 0 < valor_analogico < 0.5:  # Caso solo esteja umido
-        print("STATUS: Solo umido - {} \n".format(valor_analogico))
+        print("\nSTATUS: Solo umido - {} \n".format(valor_analogico))
 
     elif 0.5 < valor_analogico < 0.8:  # Caso solo esteja normal
-        print("STATUS: Solo moderado - {} \n".format(valor_analogico))
+        print("\nSTATUS: Moderate soil - {} \n".format(valor_analogico))
 
     elif 0.8 < valor_analogico <= 1:  # Caso o solo esteja seco
-        print(" \nSTATUS: Solo seco - {}\n".format(valor_analogico))
+        print("\nSTATUS: Solo seco - {}\n".format(valor_analogico))
         Board.digital[10].write(0)
         print("REGANDO...")
-        time.sleep(2)  # Mantem aberta por 2 segundos
+        time.sleep(2.5)  # Mantem aberta por 2 segundos
         Board.digital[10].write(1)  # desliga o rele
         print("PLANTAS REGADAS COM SUCESSO!!!")
 
-    time.sleep(60)  # Delay de 3 seg, depois o loop se repete
+    time.sleep(10)  # Delay de 3 seg, depois o loop se repete
